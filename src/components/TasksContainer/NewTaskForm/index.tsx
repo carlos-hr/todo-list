799959cd9be3d1b1966442325f6e2ad83b9ed4b6
Carlos-hr <carlos.henrique17.chr@gmail.com>
2022-07-29 14:12:06 -0300

    feature: creating logic to add task

--- src/components/TasksContainer/NewTaskForm/index.tsx
+++ src/components/TasksContainer/NewTaskForm/index.tsx
@@ -1,11 +1,22 @@
 import { PlusCircle } from "phosphor-react";
+import { FormEvent } from "react";
 import styles from "./NewTaskForm.module.css";
 
-const NewTaskForm = () => {
+interface NewTaskFormProps {
+  addTask: (e: FormEvent) => void;
+  newTask: string;
+  setNewTask: (e: string) => void;
+}
+
+const NewTaskForm = ({ addTask, newTask, setNewTask }: NewTaskFormProps) => {
   return (
-    <form className={styles.newTaskForm}>
-      <input placeholder="Adicione uma nova tarefa" />
-      <button>
+    <form className={styles.newTaskForm} onSubmit={(e) => addTask(e)}>
+      <input
+        placeholder="Adicione uma nova tarefa"
+        onChange={(e) => setNewTask(e.target.value)}
+        value={newTask}
+      />
+      <button type="submit">
         Criar
         <PlusCircle size={16} />
       </button>
